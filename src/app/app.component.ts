6f4d6b7de37f3424c0de1728639e001c91440184
Facundo Palleros <facupls10@gmail.com>
2023-12-07 11:43:48 -0300

    Iniciando proyecto pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pipesApp';
+  public title = 'pipesApp';
+  public name = 'Facundo Palleros';
 }
