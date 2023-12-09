40489041f7bda28a3cc6f3c8283ae3e50943263a
Facundo Palleros <facupls10@gmail.com>
2023-12-09 19:18:33 -0300

    update docs

--- docs/pipes-app/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-IG23QGJG.mjs";import"./chunk-QTWYD2N2.mjs";import"./chunk-L2NAJT6F.mjs";import"./chunk-DAEAVZJZ.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
