9c81fcc2e110daaa602998f7ae64684a5bb1d653
Facundo Palleros <facupls10@gmail.com>
2023-12-11 18:26:02 -0300

    update docs con href

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-KBQVHAPX.mjs";import"./chunk-3Y6GVWAG.mjs";import"./chunk-TNTRHZWU.mjs";import"./chunk-IG56ETNG.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
